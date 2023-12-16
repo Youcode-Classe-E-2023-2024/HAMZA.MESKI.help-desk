eff11add2deda3831883a06b3e0f8ea92c1197ae
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2023-12-16 23:21:11 +0100

    Ticket Section
    .

--- controller/TicketSection.php
+++ controller/TicketSection.php
@@ -1,3 +1,36 @@
 <?php
-    
+    require_once '../model/TicketsDB.php';
+    require_once '../model/DepartementsDB.php';
+    $tickets = $tickets_database->displayTickets();
+    $departments = $departments_database->displayDepartments();
+   
+    foreach($tickets as $ticket) {
+        if($ticket->id == $_POST['ticketId']) {
+            foreach($departments as $department) {
+                print <<<HEREDOC
+                <article name="ticketId" value="$ticket->id" class="w-[80%] bg-white shadow-md p-6 rounded-lg h-[296px] cursor-pointer text-left">
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Ticket ID:</strong> $ticket->id
+                    </div>
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Subject:</strong> $ticket->subject
+                    </div>
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Department:</strong> $department->departement
+                    </div>
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Status:</strong> $ticket->status
+                    </div>
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Created by:</strong> $ticket->created_by
+                    </div>
+                    <div>
+                        <strong class="text-gray-700">Assigned to:</strong> $ticket->assigned_to
+                    </div>
+                </article>
+                HEREDOC;
+                break;
+            }
+        }
+    }
 ?>
--- view/ticketSection.php
+++ view/ticketSection.php
@@ -19,26 +19,7 @@
 
         <main class="h-[88.6%] grid grid-cols-5">
             <section class="w-full col-span-3 flex items-center px-12 bg-green-400">
-                <article name="ticketId" value="$ticket->id" class="w-[80%] bg-white shadow-md p-6 rounded-lg h-[296px] cursor-pointer text-left">
-                    <div class="mb-4">
-                        <strong class="text-gray-700">Ticket ID:</strong> $ticket->id
-                    </div>
-                    <div class="mb-4">
-                        <strong class="text-gray-700">Subject:</strong> $ticket->subject
-                    </div>
-                    <div class="mb-4">
-                        <strong class="text-gray-700">Department:</strong> $department->departement
-                    </div>
-                    <div class="mb-4">
-                        <strong class="text-gray-700">Status:</strong> $ticket->status
-                    </div>
-                    <div class="mb-4">
-                        <strong class="text-gray-700">Created by:</strong> $ticket->created_by
-                    </div>
-                    <div>
-                        <strong class="text-gray-700">Assigned to:</strong> $ticket->assigned_to
-                    </div>
-                </article>
+                <?php require_once '../controller/TicketSection.php' ?>
             </section>
             <section id="commentSection" class="col-span-2 h-full ">
 
