5e82dfd5805145f01afddb2368b6f90a4429ff3e
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2023-12-16 23:02:29 +0100

    Style Stuffs
    .

--- view/ticketSection.php
+++ view/ticketSection.php
@@ -16,7 +16,35 @@
             <div></div>
             <?php require_once '../controller/LoggerDispaly.php' ?>
         </nav>
-        
+
+        <main class="h-[88.6%] grid grid-cols-5">
+            <section class="w-full col-span-3 flex items-center px-12 bg-green-400">
+                <article name="ticketId" value="$ticket->id" class="w-[80%] bg-white shadow-md p-6 rounded-lg h-[296px] cursor-pointer text-left">
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Ticket ID:</strong> $ticket->id
+                    </div>
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Subject:</strong> $ticket->subject
+                    </div>
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Department:</strong> $department->departement
+                    </div>
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Status:</strong> $ticket->status
+                    </div>
+                    <div class="mb-4">
+                        <strong class="text-gray-700">Created by:</strong> $ticket->created_by
+                    </div>
+                    <div>
+                        <strong class="text-gray-700">Assigned to:</strong> $ticket->assigned_to
+                    </div>
+                </article>
+            </section>
+            <section id="commentSection" class="col-span-2 h-full ">
+
+            </section>
+        </main>
+
     </section>
     <!-- tailwind CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
