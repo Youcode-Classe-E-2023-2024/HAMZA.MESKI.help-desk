556776af5fb93946c083412b09d7a9f5eed9fd49
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2023-12-17 13:41:10 +0100

    Update
    .

--- controller/commentsController/DisplayComments.php
+++ controller/commentsController/DisplayComments.php
@@ -13,59 +13,75 @@
         print $comment . '<br>';
         $comments_database->insertComment($commenter_id, $ticket_id, $comment);
     }
+
+    class DisplayComments {
+        private $comments;
+        private $users;
+        private $currentUserId;
+        private $currentTicketId;
     
-    $comments = $comments_database->displayComments();
-    $users = $users_database->displayUsers();
- 
-    $html = '';
-    foreach ($comments as $comment) {
-        foreach($users as $user) {
-            if($comment->commenter_id == $user->id) {
-                if($comment->commenter_id == $_SESSION['user-id'] && $comment->ticket_id == $_SESSION['current_ticket'] ) {
-                    $html .= <<<NOWDOC
-                        <main class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
-                            <footer class="flex justify-between items-center mb-2">
-                                <div class="flex items-center">
-                                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-                                    <img class="mr-2 w-6 h-6 rounded-full" src="../images/$user->avatar" alt="testing">
-                                    $user->username
+        public function __construct($comments, $users, $currentUserId, $currentTicketId)
+        {
+            $this->comments = $comments;
+            $this->users = $users;
+            $this->currentUserId = $currentUserId;
+            $this->currentTicketId = $currentTicketId;
+        }
+
+        public function renderHTML() {
+            $html = '';
+            foreach ($this->comments as $comment) {
+                foreach($this->users as $user) {
+                    if($comment->commenter_id == $user->id) {
+                        if($comment->commenter_id == $_SESSION['user-id'] && $comment->ticket_id == $_SESSION['current_ticket'] ) {
+                            $html .= <<<NOWDOC
+                                <main class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
+                                    <footer class="flex justify-between items-center mb-2">
+                                        <div class="flex items-center">
+                                        <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                                            <img class="mr-2 w-6 h-6 rounded-full" src="../images/$user->avatar" alt="testing">
+                                            $user->username
+                                        </p>
+                                        <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
+                                                title="February 8th, 2022">Feb. 8, 2022</time>
+                                        </p>
+                                        </div>
+                                        
+                                        <button key="$comment->id" class="comment_setting text-white" type="button">
+                                            <ion-icon class="text-xl" name="ellipsis-vertical-outline"></ion-icon>
+                                        </button>
+                                    </footer>
+                                    <p class="text-gray-500 dark:text-gray-400">
+                                    $comment->comment
+                                    </p>
+                                </main>
+                            NOWDOC;
+                        }elseif ($comment->ticket_id == $_SESSION['current_ticket'] ) {
+                            $html .= <<<NOWDOC
+                            <main class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
+                                <footer class="flex justify-between items-center mb-2">
+                                    <div class="flex items-center">
+                                    <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                                        <img class="mr-2 w-6 h-6 rounded-full" src="../images/$user->avatar" alt="testing">
+                                        $user->username
+                                    </p>
+                                    <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
+                                            title="February 8th, 2022">Feb. 8, 2022</time>
+                                    </p>
+                                    </div>
+                                </footer>
+                                <p class="text-gray-500 dark:text-gray-400">
+                                    $comment->comment
                                 </p>
-                                <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
-                                        title="February 8th, 2022">Feb. 8, 2022</time>
-                                </p>
-                                </div>
-                                
-                                <button key="$comment->id" class="comment_setting text-white" type="button">
-                                    <ion-icon class="text-xl" name="ellipsis-vertical-outline"></ion-icon>
-                                </button>
-                            </footer>
-                            <p class="text-gray-500 dark:text-gray-400">
-                            $comment->comment
-                            </p>
-                        </main>
-                    NOWDOC;
-                }elseif ($comment->ticket_id == $_SESSION['current_ticket'] ) {
-                    $html .= <<<NOWDOC
-                    <main class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
-                        <footer class="flex justify-between items-center mb-2">
-                            <div class="flex items-center">
-                            <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-                                <img class="mr-2 w-6 h-6 rounded-full" src="../images/$user->avatar" alt="testing">
-                                $user->username
-                            </p>
-                            <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
-                                    title="February 8th, 2022">Feb. 8, 2022</time>
-                            </p>
-                            </div>
-                        </footer>
-                        <p class="text-gray-500 dark:text-gray-400">
-                            $comment->comment
-                        </p>
-                    </main>
-                    NOWDOC;
+                            </main>
+                            NOWDOC;
+                        }
+                    }
                 }
             }
+            return $html;
         }
     }
-    print $html;
+    $displayComments = new DisplayComments($comments_database->displayComments(), $users_database->displayUsers(), $_SESSION['user-id'], $_SESSION['current_ticket']);
+    print $displayComments->renderHTML();
 ?>
--- controller/commentsController/UpdateComments.php
+++ controller/commentsController/UpdateComments.php
@@ -1,8 +1,5 @@
 <?php 
     require_once '../../model/CommentsDB.php';
-    // print '<pre>'; 
-    // print_r($_POST); 
-    // print '</pre>';
 
     $comment_id = filter_input(INPUT_POST, 'commentId', FILTER_SANITIZE_SPECIAL_CHARS);
     $comment = filter_input(INPUT_POST, 'newComment', FILTER_SANITIZE_SPECIAL_CHARS);
