b2e4afeed30c0a429b75e832dd0b57b93397f3d0
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2023-12-21 00:00:58 +0100

    .

--- view/scripts/index.js
+++ view/scripts/index.js
@@ -1,6 +1,6 @@
-const tickets_container = document.getElementById('tickets_container');
-fetch('../controller/TicketContainer.php')
-.then(response=>response.text())
-.then(data => tickets_container.innerHTML = data);
-setInterval(function(){
-}, 20000)    
+// const tickets_container = document.getElementById('tickets_container');
+// fetch('../controller/TicketContainer.php')
+// .then(response=>response.text())
+// .then(data => tickets_container.innerHTML = data);
+// setInterval(function(){
+// }, 20000)    
