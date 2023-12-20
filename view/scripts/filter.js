ceabed6ae093abd7ac4a83c3b8ab697687912de1
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2023-12-20 21:08:09 +0100

    filter

--- view/scripts/filter.js
+++ view/scripts/filter.js
@@ -1,12 +1,41 @@
-// const myTasks = document.getElementById('myTasks'); 
-// const iCreaetd = document.getElementById('iCreated'); 
-// const all = document.getElementById('all'); 
-// const priority = document.getElementById('priority'); 
+const myTasks = document.getElementById('myTasks'); 
+const iCreaetd = document.getElementById('iCreated'); 
+const all = document.getElementById('all'); 
+const priority = document.getElementById('priority'); 
+const elementsArray = document.querySelectorAll('.TICKET');
 
-// const TICKET = document.querySelectorAll('.TICKET');
-// for (const el of TICKET) {
+// console.log(TICKET[2].textContent.split('Assigned to: ')[1].trim());// this code detect the priority it could be: very-important important low
 
-// }
-console.log(TICKET[2].textContent.split('Assigned to: ')[1].trim());// this code detect the priority it could be: very-important important low
+const container = document.getElementById('tickets_container');
+priority.addEventListener('click', function() {
+    console.log('hello');
+    const elements = container.children;
 
+    // Convert HTMLCollection to array
+    const elementsArray = Array.from(elements);
 
+    // Define custom order based on extracted priority
+    const order = { 'very-important': 1, 'important': 2, 'low': 3 };
+
+    // Sort elements based on extracted priority
+    elementsArray.sort((a, b) => {
+      const priorityA = extractPriority(a.textContent);
+      const priorityB = extractPriority(b.textContent);
+
+      return order[priorityA] - order[priorityB];
+    });
+
+    // Clear the container
+    container.innerHTML = '';
+
+    // Append sorted elements back to the container
+    elementsArray.forEach(element => {
+      container.appendChild(element);
+    });
+});
+
+  // Function to extract priority from messy text content
+function extractPriority(text) {
+    const priorityMatches = text.match(/priority:\s*([a-z-]+)/i);
+    return priorityMatches ? priorityMatches[1].toLowerCase() : '';
+}
